babc7f8e597fc6182fe9cf764fe021818d8f38f9
Amay Devshatwar <amaydevshatwar@gmail.com>
2026-07-18 20:07:15 +0530

    Initial Spring Boot project setup

---
+++ src/main/java/com/amay/expenseflow/ExpenseflowBackendApplication.java
@@ -0,0 +1,13 @@
+package com.amay.expenseflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseflowBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseflowBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amay/expenseflow/ExpenseflowBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amay.expenseflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseflowBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
